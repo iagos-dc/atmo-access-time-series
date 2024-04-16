4d0a877c6871cb0cb9f53fab330e2b22c030baa1
dboulang <dboulang@UALL240414>
2024-04-16 11:32:56 +0200

    final version

--- data_access/query_iagos.py
+++ data_access/query_iagos.py
@@ -3,10 +3,10 @@ from requests.exceptions import HTTPError
 import xarray as xr  
 
 REST_URL_PATH = "https://services.iagos-data.fr/prod/v2.0/"
-REST_URL_STATIONS = "https://api.sedoo.fr/aeris-catalogue-prod/metadata/3228ee61-f23e-452d-a06b-ffe060f3b728"
-REST_URL_REGIONS = "https://api.sedoo.fr/aeris-catalogue-prod/metadata/77642149-2427-491e-b3a5-79b09cca241a"
+REST_URL_STATIONS = REST_URL_PATH + "airports/l3"
+REST_URL_REGIONS = REST_URL_PATH + "regions"
 REST_URL_VARIABLES = REST_URL_PATH + "parameters/public"
-REST_URL_SEARCH = REST_URL_PATH + "l3/searchNetcdfFilenames"
+REST_URL_SEARCH = REST_URL_PATH + "l3/search?codes="
 REST_URL_DOWNLOAD = REST_URL_PATH + "l3/loadNetcdfFile"
 
 STATIC_PARAMETERS = ["latitude", "longitude", "air_pressure", "barometric_altitude"]
@@ -53,13 +53,7 @@ def get_list_platforms():
     try:
         response = requests.get(REST_URL_STATIONS)
         response.raise_for_status()
-        jsonResponse = response.json()
-        ret = []
-        for airport in jsonResponse['spatialExtents']:
-            station = { 'short_name': airport['name'].split(", ")[0], 'long_name': airport['name'].split(", ")[1],
-                       'longitude': airport['area']['longitude'], 'latitude': airport['area']['latitude'], 'altitude': airport['area']['altitude']  }
-            ret.append(station)
-        return ret    
+        return response.json()
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
     except Exception as err:
@@ -70,14 +64,7 @@ def get_list_regions():
     try:
         response = requests.get(REST_URL_REGIONS)
         response.raise_for_status()
-        jsonResponse = response.json()
-        ret = []
-        for region in jsonResponse['spatialExtents']:
-            station = { 'short_name': region['name'], 'long_name': region['description'],
-                       'longitude_min': region['area']['westLongitude'], 'longitude_max': region['area']['eastLongitude'],
-                       'latitude_min': region['area']['southLatitude'], 'latitude__max': region['area']['northLatitude']   }
-            ret.append(station)
-        return ret     
+        return response.json()
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
     except Exception as err:
@@ -109,14 +96,10 @@ def query_datasets(variables_list, temporal_extent, spatial_extent):
 
 def query_datasets_stations(codes): 
     try:
-        url = REST_URL_SEARCH + "?codes=" + codes
+        url = REST_URL_SEARCH + codes
         response = requests.get(url)
         response.raise_for_status()
-        jsonResponse = response.json()
-        ret = []
-        for item in jsonResponse:
-            ret.append(item)
-        return ret    
+        return response.json()
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
     except Exception as err:
@@ -124,6 +107,7 @@ def query_datasets_stations(codes):
 
 
 def read_dataset(dataset_id, variables_list, temporal_extent, spatial_extent):
+    print(REST_URL_DOWNLOAD + "?fileId=" + dataset_id.replace("#", "%23"))
     results = requests.get(REST_URL_DOWNLOAD + "?fileId=" + dataset_id.replace("#", "%23"))
     with open('/tmp/fic.nc', 'wb') as f:
         f.write(results.content)
@@ -137,11 +121,13 @@ def read_dataset(dataset_id, variables_list, temporal_extent, spatial_extent):
 
 
 if __name__ == "__main__":
-    print(get_list_platforms())
-    print(get_list_regions())
-    print(get_list_variables())
-    print(query_datasets_stations('FRA,NAt'))
-    for dataset in query_datasets_stations('FRA,NAt'):
-        array = read_dataset(dataset, ['mole_fraction_of_carbon_monoxide_in_air', 'mole_fraction_of_ozone_in_air' ], None, None)
-        print(array['CO_mean'][0:10])
+    # print(get_list_platforms())
+    # print(get_list_regions())
+    # print(get_list_variables())
+    # print(query_datasets_stations("FRA,NAt"))
+    for dataset in query_datasets_stations("FRA,NAt"):
+        for url in dataset['urls']:
+            if url['type'] == "LANDING_PAGE":
+                array = read_dataset(url['url'], ['mole_fraction_of_carbon_monoxide_in_air', 'mole_fraction_of_ozone_in_air' ], None, None)
+                print(array['CO_mean'][0:10])
 
